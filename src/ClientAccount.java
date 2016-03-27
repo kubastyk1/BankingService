0b44b366be82996491875f12d2fa2d717c06eb5a
kubastyk1 <kubastyk@wp.pl>
2016-03-27 19:05:29 +0200

    readFile() delete comment

--- src/ClientAccount.java
+++ src/ClientAccount.java
@@ -159,42 +159,6 @@ public class ClientAccount {
 		  } /*finally {
 		    bufferedReader.close();
 		  }*/
-		
-		
-		
-		
-		/* FileReader fr = null;
-	      LineNumberReader lnr = null;
-	      String str;
-	      int i;
-	      
-	      try{
-	         // create new reader
-	         fr = new FileReader(filePath);
-	         lnr = new LineNumberReader(fr);
-	   
-	         // read lines till the end of the stream
-	         while((str=lnr.readLine())!=null)
-	         {
-	            i=lnr.getLineNumber();
-	            System.out.print("("+i+")");
-	                  
-	            // prints string
-	            System.out.println(str);
-	            lnr.setLineNumber(i + 5);
-	         }
-	      }catch(Exception e){
-	         
-	         // if any error occurs
-	         e.printStackTrace();
-	      }/*finally{
-	         
-	         // closes the stream and releases system resources
-	         if(fr!=null)
-	            fr.close();
-	         if(lnr!=null)
-	            lnr.close();
-	      }*/
 	   }
 	
 	
